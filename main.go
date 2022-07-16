7de1da3f694d3191bbcac75958bde9054ccc48ae
wall-e <caojian816@163.com>
2022-07-16 20:03:18 +0800

    配置方案的实现

--- main.go
+++ main.go
@@ -2,17 +2,29 @@ package main
 
 import (
 	"blog-service/bootstrap"
+	btsConfig "blog-service/config"
+	"blog-service/pkg/config"
+	"flag"
 	"fmt"
 	"github.com/gin-gonic/gin"
 )
 
+func init() {
+	// 加载 config 目录下的配置信息
+	btsConfig.Initialize()
+}
 func main() {
+	// 配置初始化，依赖命令行 --env 参数
+	var env string
+	flag.StringVar(&env, "env", "", "加载 .env 文件，如 --env=testing 加载的是 .env.testing 文件")
+	flag.Parse()
+	config.InitConfig(env)
 	//new 一个Gin Engine 实例
 	r := gin.New()
 	// 初始化路由绑定
 	bootstrap.SetupRoute(r)
 	//运行服务，默认为 8080，我们指定端口为 8000
-	err := r.Run(":8000")
+	err := r.Run(":" + config.Get("app.port"))
 	if err != nil {
 		// 错误处理，端口被占用了或者其他错误
 		fmt.Println(err.Error())
