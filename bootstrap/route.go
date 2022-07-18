f7d0ddbe1d8ccc69fb4caaadc43b6891ac2776ce
liachange <caojian816@163.com>
2022-07-18 17:02:44 +0800

    Recovery 日志记录

--- bootstrap/route.go
+++ bootstrap/route.go
@@ -18,7 +18,7 @@ func SetupRoute(router *gin.Engine) {
 	setup404Handler(router)
 }
 func registerGlobalMiddleWare(router *gin.Engine) {
-	router.Use(middlewares.Logger(), gin.Recovery())
+	router.Use(middlewares.Logger(), middlewares.Recovery())
 }
 
 func setup404Handler(router *gin.Engine) {
