656e38d406f06e1ecbe7ec63dd736602eb84d422
liachange <caojian816@163.com>
2022-08-04 10:11:55 +0800

    发送邮件(mail包)

---
+++ pkg/mail/mail.go
@@ -0,0 +1,41 @@
+package mail
+
+import (
+	"blog-service/pkg/config"
+	"sync"
+)
+
+type From struct {
+	Address string
+	Name    string
+}
+
+type Email struct {
+	From    From
+	To      []string
+	Bcc     []string
+	CC      []string
+	Subject string
+	Text    []byte
+	HTML    []byte
+}
+type Mailer struct {
+	Driver Driver
+}
+
+var once sync.Once
+var internalMailer *Mailer
+
+// NewMailer 单例模式获取
+func NewMailer() *Mailer {
+	once.Do(func() {
+		internalMailer = &Mailer{
+			Driver: &SMTP{},
+		}
+	})
+	return internalMailer
+}
+
+func (mailer *Mailer) Send(email Email) bool {
+	return mailer.Driver.Send(email, config.GetStringMapString("mail.smtp"))
+}
