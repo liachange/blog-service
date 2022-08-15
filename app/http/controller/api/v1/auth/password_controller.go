ce3fe67ab9593b8569fece0ed5ea41eb5d502581
liachange <caojian816@163.com>
2022-08-15 09:57:05 +0800

    通过邮箱找回密码

--- app/http/controller/api/v1/auth/password_controller.go
+++ app/http/controller/api/v1/auth/password_controller.go
@@ -15,12 +15,29 @@ type PasswordController struct {
 
 func (pc *PasswordController) RestByPhone(c *gin.Context) {
 	// 1. 验证表单
-	request := requests.RestPasswordRequest{}
+	request := requests.ResetPasswordRequest{}
 	if ok := requests.Validate(c, &request, requests.RestByPhone); !ok {
 		return
 	}
 	// 2. 更新密码
 	userModel := user.GetByPhone(request.Phone)
+	if userModel.ID == 0 {
+		response.Abort404(c)
+	} else {
+		userModel.Password = request.Password
+		userModel.Save()
+		response.Success(c)
+	}
+}
+
+func (pc *PasswordController) ResetByEmail(c *gin.Context) {
+	// 1. 验证表单
+	request := requests.ResetByEmailRequest{}
+	if ok := requests.Validate(c, &request, requests.ResetByEmail); !ok {
+		return
+	}
+	// 2. 更新密码
+	userModel := user.GetByEmail(request.Email)
 	if userModel.ID == 0 {
 		response.Abort404(c)
 	} else {
--- app/requests/password_request.go
+++ app/requests/password_request.go
@@ -6,7 +6,7 @@ import (
 	"github.com/thedevsaddam/govalidator"
 )
 
-type RestPasswordRequest struct {
+type ResetPasswordRequest struct {
 	Phone      string `json:"phone,omitempty" valid:"phone"`
 	VerifyCode string `json:"verify_code,omitempty" valid:"verify_code"`
 	Password   string `json:"password,omitempty" valid:"password"`
@@ -33,7 +33,39 @@ func RestByPhone(data interface{}, c *gin.Context) map[string][]string {
 		},
 	}
 	errs := validate(data, rules, messages)
-	_data := data.(*RestPasswordRequest)
+	_data := data.(*ResetPasswordRequest)
 	errs = validators.ValidateVerifyCode(_data.Phone, _data.VerifyCode, errs)
+	return errs
+}
+
+type ResetByEmailRequest struct {
+	Email      string `json:"email,omitempty" valid:"email"`
+	VerifyCode string `json:"verify_code,omitempty" valid:"verify_code"`
+	Password   string `json:"password,omitempty" valid:"password"`
+}
+
+func ResetByEmail(data interface{}, c *gin.Context) map[string][]string {
+	rules := govalidator.MapData{
+		"email":       []string{"required", "email"},
+		"verify_code": []string{"required", "digits:6"},
+		"password":    []string{"required", "min:6"},
+	}
+	messages := govalidator.MapData{
+		"email": []string{
+			"required:邮箱为必填项，参数名称为 email",
+			"email:邮箱 格式不正确，请提供有效的邮箱地址",
+		},
+		"verify_code": []string{
+			"required:验证码为必填项，参数名称为 required",
+			"digits:验证码长度需为6位",
+		},
+		"password": []string{
+			"required:密码为必填项，参数名称为 password",
+			"min:密码长度需大于6位",
+		},
+	}
+	errs := validate(data, rules, messages)
+	_data := data.(*ResetByEmailRequest)
+	errs = validators.ValidateVerifyCode(_data.Email, _data.VerifyCode, errs)
 	return errs
 }
